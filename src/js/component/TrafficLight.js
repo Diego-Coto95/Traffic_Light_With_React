fd7d36dd0e531678b604318654f793b409654c7c
Juan Diego Coto <jd506@hotmail.com>
2021-02-08 17:50:14 +0000

    Adding funtionalities to the traffic light

--- src/js/component/TrafficLight.js
+++ src/js/component/TrafficLight.js
@@ -1,17 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
-export function TrafficLight() {
+export const TrafficLight = () => {
+	let [red, setRed] = useState("red");
+	let [yellow, setYellow] = useState("yellow");
+	let [green, setGreen] = useState("green");
+
+	let redColor = () => {
+		if (red == "red") {
+			setRed("red color");
+		}
+		setYellow("yellow");
+		setGreen("green");
+	};
+
+	let yellowColor = () => {
+		if (yellow == "yellow") {
+			setYellow("yellow color");
+		}
+		setRed("red");
+		setGreen("green");
+	};
+
+	let greenColor = () => {
+		if (green == "green") {
+			setGreen("green color");
+		}
+		setRed("red");
+		setYellow("yellow");
+	};
+
 	return (
 		<div className="content">
 			<div className="square">
-				<div className="circle bg-danger"></div>
+				<div className={red} onClick={redColor}></div>
 			</div>
 			<div className="square">
-				<div className="circle bg-warning"></div>
+				<div className={yellow} onClick={yellowColor}></div>
 			</div>
 			<div className="square">
-				<div className="circle bg-success"></div>
+				<div className={green} onClick={greenColor}></div>
 			</div>
 		</div>
 	);
-}
+};
--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 //import react into the bundle
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
 //include bootstrap npm library into the bundle
@@ -10,6 +10,7 @@ import "../styles/index.scss";
 
 //import your own components
 import { Home } from "./component/home.js";
+import { TrafficLight } from "./component/TrafficLight.js";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<TrafficLight />, document.querySelector("#app"));
